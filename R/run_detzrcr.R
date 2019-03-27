211fbd748dd6ce7f65f51355b0dc3573171bf3d5
Magnus Kristoffersen <magnus.kristoffersen@geo.uio.no>
2019-03-27 10:03:53 +0100

    Pass arguments on to shiny::runApp.

--- R/run_detzrcr.R
+++ R/run_detzrcr.R
@@ -1,8 +1,9 @@
 #' Launch shiny interface
 #'
+#'@param ... Pass arguments on to shiny::runApp
 #' @export
 #'
-run_detzrcr <- function() {
+run_detzrcr <- function(...) {
   app_dir <- system.file('shiny-apps', 'detzrcr_app', package = 'detzrcr')
-  shiny::runApp(app_dir, display.mode = "normal")
+  shiny::runApp(app_dir, display.mode = "normal", ...)
 }
